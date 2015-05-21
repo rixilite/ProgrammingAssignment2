81d915a851c6645d30039ba15613340bcc0352c7
rixilite <rcbrideau@gmail.com>
2015-05-21 11:37:34 -0400

    Assignment 2 script
    Submitted cachematrix.R script 5/21/15

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions leverage abstraction in order to 
+## provide a means to cache values for later use.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function generates a list of functions as a 
+## means to act on an instance of an object.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(MatrixIn = matrix()) {
+    MatrixOut <- NULL
+    set <- function(y) {
+        MatrixIn <<- y
+        MatrixOut <<- NULL
+    }
+    get <- function() {
+        MatrixIn
+    }
+    setInverse <- function(InverseVal) {
+        MatrixOut <<- InverseVal
+    }
+    getInverse <- function() {
+        MatrixOut
+    }
+    list(set = set, 
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse
+    )
 }
 
+## This function will get and return a stored inverse matrix if it exists.  
+## If it does not exist, the function will solve for the inverse, store it
+## and return it.
 
-## Write a short comment describing this function
+cacheSolve <- function(MatrixIn, ...) {
+    ## Return a matrix that is the inverse of 'MatrixIn'
+    InvMatrix <- MatrixIn$getInverse()
+    if(!is.null(InvMatrix)) {
+        message("getting cached data")
+        return(InvMatrix)
+    }
+    MatrixData <- MatrixIn$get()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    InvMatrix <- solve(MatrixData)
+    MatrixIn$setInverse(InvMatrix)
+    InvMatrix
 }
+
